ef92a056e4e5c2f10c37c418ed30ad24ac9458cb
JokerAlex <alex.zhao1023@gmail.com>
2018-11-10 15:16:40 +0800

    update

---
+++ src/main/java/org/ylgzs/info/aspect/WebLogAspect.java
@@ -0,0 +1,10 @@
+package org.ylgzs.info.aspect;
+
+/**
+ * @ClassName WebLogAspect
+ * @Description TODO
+ * @Author alex
+ * @Date 2018/11/10
+ **/
+public class WebLogAspect {
+}
---
+++ src/main/java/org/ylgzs/info/schedule/Task.java
@@ -0,0 +1,10 @@
+package org.ylgzs.info.schedule;
+
+/**
+ * @ClassName Task
+ * @Description TODO
+ * @Author alex
+ * @Date 2018/11/10
+ **/
+public class Task {
+}
